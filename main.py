3bda2eb5c8dabd15d8ff12b9a8d052e810a402f4
Amon D <martinhost003@gmail.com>
2024-01-07 19:44:16 -0500

    ...

--- main.py
+++ main.py
@@ -1,40 +1,179 @@
-from argparse import ArgumentParser, Namespace
-import subprocess
+import argparse
 import re
+import subprocess
+import sys
+from argparse import ArgumentParser, Namespace
+
+###############################################
+#
+#
+#
+#           CONFIGGGGGGGGGGG
+#
+#
+#
+#
+#
+###############################################
 
 
-def search_spotdl(search_term):
-    command = ['spotdl', search_term]
-    result = subprocess.run(command, capture_output=True, text=True)
-    output = result.stdout
+#### Replace the \ with \\ in path names
 
-    # Find url
-    match = re.search(r'https?://[^\s/$.?#].[^\s]*', output)
-    if match:
-        return match.group(0)
+spotdl_path = "A:\\Apps\\Programs\\Python\\Python312\\Scripts\\spotdl.exe"
+ytdlp_path = "A:\\Apps\\Programs\\Python\\Python312\\Scripts\\yt-dlp.exe"
 
-    return None
 
-def download_music(url):
-    command = ['yt-dlp', '-f', '251/140/bestaudio', '--embed-thumbnail', '--add-metadata', '-o', "%(title)s.%(ext)s", '-ciw', '-x', '--audio-quality', '0', '--audio-format', 'mp3', url]
-    subprocess.run(command)
+###############################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument("user_input", help="Song search term, URL, etc.")
+args: Namespace = parser.parse_args()
+
+
+def check_input(user_input):
+    pattern1 = r"https://open\.spotify\.com/.*"
+    pattern2 = r"open\.spotify\.com/.*"
+    pattern3 = r"spotify\.com/.*"
+
+    if (
+        re.match(pattern1, user_input)
+        or re.match(pattern2, user_input)
+        or re.match(pattern3, user_input)
+    ):
+        return "Spotify URL"
+
+    # General URL
+    url_pattern = r"https?://(?:www\.)?\w+\.\w+(/\S*)?"
+    if re.match(url_pattern, user_input):
+        return "General URL"
+
+    # Search term
+    return "Search Term"
+
+
+user_input = args.user_input
+input_type = check_input(user_input)
+
 
 def main():
-    parser = ArgumentParser()
-    parser.add_argument("search_term", help="Song name, url, playlist url, etc.")
-    args: Namespace = parser.parse_args()
+    if input_type == "Spotify URL":
+        command = [spotdl_path, user_input]
+        run_command_no(command)
 
-    # spot search
-    song_url = search_spotdl(search_term)
+    elif input_type == "General URL":
+        command = [
+            ytdlp_path,
+            "-f",
+            "251/140/bestaudio",
+            "--embed-thumbnail",
+            "--add-metadata",
+            "-o",
+            "%(title)s.%(ext)s",
+            "-ciw",
+            "-x",
+            "--audio-quality",
+            "0",
+            "--audio-format",
+            "mp3",
+            user_input,
+        ]
 
-    if song_url is None:
-        print('Song found on Spotify:)
-        user_input = input('Is this the correct song? (y/n): ')
-        if user_input.lower() == 'y':
-            download_music(song_url)
-        else:
-            print('Trying with yt-dlp...')
-            download_music(search_term)
+        run_command_no(command)
 
-if __name__ == '__main__':
-    main()
+    else:
+        while True:
+            which_tool = input(
+                "No URL was provided, which tool do you want to search for a song with? (spotdl, ytdlp)?: "
+            )
+            if which_tool is None:
+                continue
+            elif which_tool == "spotdl":
+                break
+            elif which_tool == "ytdlp":
+                break
+            else:
+                continue
+        if which_tool == "spotdl":
+            command = [spotdl_path, user_input]
+            run_command_no(command)
+        elif which_tool == "ytdlp":
+            command = [
+                ytdlp_path,
+                "ytsearch10:" + user_input,
+                "--get-id",
+                "--get-title",
+            ]
+
+            output, return_code = run_command_wo(command)
+
+            lines = output.split("\n")
+
+            for i, line in enumerate(lines):
+                print(f"{i+1}. {line}")
+            selected_number = int(input("Which video do you want to download?: "))
+            if 1 <= selected_number <= len(lines):
+                selected_video = lines[selected_number - 1].split()[0]
+                command = [
+                    ytdlp_path,
+                    "-f",
+                    "251/140/bestaudio",
+                    "--embed-thumbnail",
+                    "--add-metadata",
+                    "-o",
+                    "%(title)s.%(ext)s",
+                    "-ciw",
+                    "-x",
+                    "--audio-quality",
+                    "0",
+                    "--audio-format",
+                    "mp3",
+                    selected_video,
+                ]
+                run_command_no(command)
+            else:
+                print("Invalid selection.")
+
+
+def run_command_no(command):
+    process = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        bufsize=1,
+        universal_newlines=True,
+    )
+
+    while True:
+        output = process.stdout.readline()
+        if output == "" and process.poll() is not None:
+            break
+        if output:
+            print(output.strip(), flush=True)
+
+    rc = process.poll()
+
+    return rc
+
+
+def run_command_wo(command):
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True
+    )
+
+    output = ""
+
+    # Read the output while the command is running
+    for line in process.stdout:
+        line = line.strip()
+        output += line + "\n"
+
+    # Get the remaining output
+    remaining_output = process.communicate()[0]
+    if remaining_output:
+        output += remaining_output.strip() + "\n"
+        print(remaining_output.strip(), flush=True)
+
+    return output, process.returncode
+
+
+main()
+
